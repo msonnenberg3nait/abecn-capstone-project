6254ee6ae48d4cc56ed7036dc7757ffa4bf6cebf
tmsimon8 <116934621+tmsimon8@users.noreply.github.com>
2023-03-10 14:37:11 -0700

    Adds remaining HTML for homepage

--- abecn/resources/views/home.blade.php
+++ abecn/resources/views/home.blade.php
@@ -56,6 +56,88 @@
         </div>
     </header>
     <main>
+        <section>
+            <div>
+                <h1>ALBERTA BUILDING ENVELOPE COUNCIL NORTH</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                <a href="#">BECOME A MEMBER</a>
+            </div>
+        </section>
+        <section>
+            <div>
+                <h2>WELCOME TO ABEC NORTH</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam, repellat. Libero, incidunt, delectus sapiente cum officia, reprehenderit rem animi quis assumenda amet sed? Eos quibusdam quas quo repellat dicta soluta!</p>
+                <a href="#">ABOUT US</a>
+            </div>
+        </section>
+        <section>
+            <div>
+                <h2>LATEST EVENTS</h2>
+                <article>
+                    <div>
+                        {{-- Image --}}
+                    </div>
+                    <div>
+                        <h3>TITLE HERE</h3>
+                        <p>02/05/2023 | By Author</p>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus laborum consectetur atque nobis repellendus nisi quam blanditiis sequi, iure error...</p>
+                        <a href="#">READ MORE</a>
+                    </div>
+                </article>
+                <article>
+                    <div>
+                        {{-- Image --}}
+                    </div>
+                    <div>
+                        <h3>TITLE HERE</h3>
+                        <p>02/05/2023 | By Author</p>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus laborum consectetur atque nobis repellendus nisi quam blanditiis sequi, iure error...</p>
+                        <a href="#">READ MORE</a>
+                    </div>
+                </article>
+                <article>
+                    <div>
+                        {{-- Image --}}
+                    </div>
+                    <div>
+                        <h3>TITLE HERE</h3>
+                        <p>02/05/2023 | By Author</p>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus laborum consectetur atque nobis repellendus nisi quam blanditiis sequi, iure error...</p>
+                        <a href="#">READ MORE</a>
+                    </div>
+                </article>
+            </div>
+        </section>
+        <section>
+            <div>
+                <h2>OUR SPONSORS</h2>
+                <div>
+                    {{-- Images --}}
+                </div>
+                <a href="#">MORE DETAILS</a>
+            </div>
+        </section>
+        <footer>
+            <div>
+                <a href="#">
+                    <img src="" alt="ABECN logo">
+                </a>
+                <ul>
+                    <li><a href="#">PRIVACY</a></li>
+                    <li><a href="#">COPYRIGHT</a></li>
+                    <li><a href="#">SITEMAP</a></li>
+                    <li><a href="#">CONTACT US</a></li>
+                </ul>
+                <form action="#">
+                    <h3>SIGN UP FOR OUR NEWSLETTER</h3>
+                    <label for="name">Full Name</label>
+                    <input type="text" name="name">
+                    <label for="email">Email</label>
+                    <input type="email" name="email">
+                    <input type="submit" name="submit" value="SIGN UP">
+                </form>
+            </div>
+        </footer>
         @foreach ($users as $user)
 
             <p>{{ $user->first_name }}</p>
