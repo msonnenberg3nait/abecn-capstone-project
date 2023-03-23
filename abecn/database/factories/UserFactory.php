1d8b49b949c7ae941022aa5c9ea693f003122853
DeniseAI <deniseisabelo16@gmail.com>
2023-03-22 23:02:50 -0600

    Revert "Changing DB credentials"
    This reverts commit a6d349716c9410fb66eac03ef8927d133ded8f72.

--- abecn/database/factories/UserFactory.php
+++ abecn/database/factories/UserFactory.php
@@ -5,23 +5,21 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
 class UserFactory extends Factory
 {
     /**
      * Define the model's default state.
      *
-     * @return array
+     * @return array<string, mixed>
      */
-    public function definition()
+    public function definition(): array
     {
         return [
-<<<<<<< Updated upstream
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-=======
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
->>>>>>> Stashed changes
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
@@ -30,15 +28,11 @@ class UserFactory extends Factory
 
     /**
      * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
      */
-    public function unverified()
+    public function unverified(): static
     {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
     }
 }
