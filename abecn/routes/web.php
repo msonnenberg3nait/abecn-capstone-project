95c0b060500236cb3b358cc9fdd3dede3a2c1d00
tsimon8 <tsimon8@nait.ca>
2023-03-27 22:34:33 -0600

    Login and registration

--- abecn/routes/web.php
+++ abecn/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Group;
+use App\Http\Controllers\ProfileController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +21,16 @@ Route::get('/', function () {
     return view('home', [
         'users' => $user
     ]);
+})->name('home');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::view('/registration', 'registration');
+require __DIR__.'/auth.php';
