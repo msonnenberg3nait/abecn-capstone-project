9b483dfec947e49ac8957af0f429ea3a683d73bc
DeniseAI <deniseisabelo16@gmail.com>
2023-03-29 20:28:56 -0600

    Create contact.blade.php

---
+++ abecn/resources/views/contact.blade.php
@@ -0,0 +1,40 @@
+<x-layout>
+    <main>
+        <section class="contact-banner">
+            <h1>Contact</h1>
+        </section>
+        <section class="get-in-touch">
+            <div class="address">
+                <img src="" alt="">
+                <p>Lorem ipsum dolor sit amet, consectetur. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
+            </div>
+            <div class="address">
+                <img src="" alt="">
+                <p>Lorem ipsum dolor sit amet, consectetur. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
+            </div>
+            <div class="address">
+                <img src="" alt="">
+                <p>Lorem ipsum dolor sit amet, consectetur. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
+            </div>
+        </section>
+        <form action="#" method="post">
+            <div>
+                <label for="firstname">First Name</label>
+                <input type="text" name="firstname" id="firstname">
+            </div>
+            <div>
+                <label for="lastname">Last Name</label>
+                <input type="text" name="lastname" id="lastname">
+            </div>
+            <div>
+                <label for="subject">Subject</label>
+                <input type="text" name="subject" id="subject">
+            </div>
+            <div>
+                <label for="message">Your Message</label>
+                <input type="textarea" name="message" id="message">
+            </div>
+            <input type="submit" value="Send Message">
+        </form>
+    </main>
+</x-layout>
