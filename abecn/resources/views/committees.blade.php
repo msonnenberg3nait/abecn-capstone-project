68f704ea92a9800448386e15cd8e3cac5b8f657c
DeniseAI <deniseisabelo16@gmail.com>
2023-03-29 20:28:38 -0600

    Create committees.blade.php

---
+++ abecn/resources/views/committees.blade.php
@@ -0,0 +1,25 @@
+<x-layout>
+    <main>
+        <section class="committees-banner">
+            <h1>Committees</h1>
+        </section>
+
+        <section class="publication">
+            <div class="publication-text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="publication-img">
+
+            </div>
+        </section>
+        <section class="casual-nights">
+            <div class="casual-nights-img">
+                
+            </div>
+            <div class="casual-nights-text">
+                <h2>Casual Nights Committee</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </section>
+    </main>
+</x-layout>
