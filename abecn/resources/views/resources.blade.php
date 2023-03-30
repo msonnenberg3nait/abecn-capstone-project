f18c849f0c093ac85acda5c878bb48ab1418d06c
DeniseAI <deniseisabelo16@gmail.com>
2023-03-29 20:29:07 -0600

    Create resources.blade.php

---
+++ abecn/resources/views/resources.blade.php
@@ -0,0 +1,90 @@
+<x-layout>
+    <main>
+        <section class="resources banner">
+            <h1>Resources</h1>
+        </section>
+
+        <section class="links">
+            <div class="links-text">
+                <h2>Links</h2>
+                <section>
+                    <ul class="category-1">
+                        <h3>Link Category Name</h3>
+                        <li><a href="#">link name here. Lorem Ipsum dolor sit amet, consectur</a></li>
+                        <li><a href="#">link name here. Lorem Ipsum dolor sit amet, consectur</a></li>
+                        <li><a href="#">link name here. Lorem Ipsum dolor sit amet, consectur</a></li>
+                    </ul>
+                </section>
+                <section>
+                    <ul class="category-1">
+                        <h3>Link Category Name</h3>
+                        <li><a href="#">link name here. Lorem Ipsum dolor sit amet, consectur</a></li>
+                        <li><a href="#">link name here. Lorem Ipsum dolor sit amet, consectur</a></li>
+                        <li><a href="#">link name here. Lorem Ipsum dolor sit amet, consectur</a></li>
+                    </ul>
+                </section>
+                <section>
+                    <ul class="category-1">
+                        <h3>Link Category Name</h3>
+                        <li><a href="#">link name here. Lorem Ipsum dolor sit amet, consectur</a></li>
+                        <li><a href="#">link name here. Lorem Ipsum dolor sit amet, consectur</a></li>
+                        <li><a href="#">link name here. Lorem Ipsum dolor sit amet, consectur</a></li>
+                    </ul>
+                </section>
+            </div>
+            <div class="img-overlap">
+
+            </div>
+        </section>
+
+        <section class="latest-publications">
+            <div><h3>Spring 2021</h3></div>
+            <div><h3>Fall 2021</h3></div>
+            <div><h3>Spring 2022</h3></div>
+            <div><h3>Fall 2022</h3></div>
+
+            <button>
+                <a href="#">Read More</a>
+            </button>
+        </section>
+
+        <section class="latest-events">
+            <div>
+                <h2>LATEST EVENTS</h2>
+                <article>
+                    <div>
+                        {{-- Image --}}
+                    </div>
+                    <div>
+                        <h3>TITLE HERE</h3>
+                        <p>02/05/2023 | By Author</p>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus laborum consectetur atque nobis repellendus nisi quam blanditiis sequi, iure error...</p>
+                        <a href="#">READ MORE</a>
+                    </div>
+                </article>
+                <article>
+                    <div>
+                        {{-- Image --}}
+                    </div>
+                    <div>
+                        <h3>TITLE HERE</h3>
+                        <p>02/05/2023 | By Author</p>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus laborum consectetur atque nobis repellendus nisi quam blanditiis sequi, iure error...</p>
+                        <a href="#">READ MORE</a>
+                    </div>
+                </article>
+                <article>
+                    <div>
+                        {{-- Image --}}
+                    </div>
+                    <div>
+                        <h3>TITLE HERE</h3>
+                        <p>02/05/2023 | By Author</p>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus laborum consectetur atque nobis repellendus nisi quam blanditiis sequi, iure error...</p>
+                        <a href="#">READ MORE</a>
+                    </div>
+                </article>
+            </div>
+        </section>
+    </main>
+</x-layout>
