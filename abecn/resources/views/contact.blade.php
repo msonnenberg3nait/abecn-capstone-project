85ab3b092938ffe58587c9966566da912ef385d7
DeniseAI <deniseisabelo16@gmail.com>
2023-04-02 23:12:23 -0600

    Update contact.blade.php

--- abecn/resources/views/contact.blade.php
+++ abecn/resources/views/contact.blade.php
@@ -8,23 +8,19 @@
                 <img src="" alt="">
                 <p>Lorem ipsum dolor sit amet, consectetur. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
             </div>
-            <div class="address">
+            <div class="phone">
                 <img src="" alt="">
                 <p>Lorem ipsum dolor sit amet, consectetur. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
             </div>
-            <div class="address">
+            <div class="email">
                 <img src="" alt="">
                 <p>Lorem ipsum dolor sit amet, consectetur. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
             </div>
         </section>
         <form action="#" method="post">
             <div>
-                <label for="firstname">First Name</label>
-                <input type="text" name="firstname" id="firstname">
-            </div>
-            <div>
-                <label for="lastname">Last Name</label>
-                <input type="text" name="lastname" id="lastname">
+                <label for="yourname">Your Name</label>
+                <input type="text" name="yourname" id="yourname">
             </div>
             <div>
                 <label for="subject">Subject</label>
