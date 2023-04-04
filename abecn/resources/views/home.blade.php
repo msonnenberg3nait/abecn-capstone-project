aa4b08b6deed3cf4a5312207c8289ddc0c0b31b3
DeniseAI <deniseisabelo16@gmail.com>
2023-04-04 13:25:51 -0600

    Update home.blade.php
    adding styles/layout

--- abecn/resources/views/home.blade.php
+++ abecn/resources/views/home.blade.php
@@ -1,13 +1,13 @@
 <x-layout>
     <main>
-        <section class="w-full bg-cover bg-center bg-home mb-9" style="height:16rem;">
+        <section class="w-full bg-cover bg-center bg-home mb-9" style="height:27rem;">
             <div class="flex items-center text-center justify-center align-middle h-full w-full bg-gray-900 bg-opacity-50 background-image: {{ asset('img/skyscrapers.jpg') }};"> 
                 <div class="banner-text inner-container">
                     <div class="mb-4">
                         <h1>ALBERT BUILDING ENVELOPE COUNCIL NORTH</h1>
                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
                     </div>
-                    <a href="#" class="mt-4 px-4 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded focus:outline-none">BECOME A MEMBER</a>
+                    <a href="#" class="home-btn mt-4 px-4 py-2 text-white text-sm uppercase font-medium rounded focus:outline-none">BECOME A MEMBER</a>
                 </div>
             </div>
         </section>
@@ -18,7 +18,7 @@
                 <a href="#">ABOUT US</a>
             </div>
             <div class="welcome-img">
-                <img src=".../public/img/audience.jpg" alt="Silhoutte of presentation">
+                <img src="{{ asset('img/presentation.png') }}" alt="Silhoutte of presentation">
             </div>
         </section>
         <section class="inner-container">
@@ -27,7 +27,7 @@
                 <div class="event-img">
                     <img src="{{ asset('img/group-work-laptops.jpg') }}" alt="Top view of a group working on a table">
                 </div>
-                <div class="">
+                <div class="event-text">
                     <h3>TITLE HERE</h3>
                     <p>02/05/2023 | By Author</p>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus laborum consectetur atque nobis repellendus nisi quam blanditiis sequi, iure error...</p>
@@ -38,7 +38,7 @@
                 <div class="event-img">
                     <img src="{{ asset('img/lecture.jpg') }}" alt="ABECN logo">
                 </div>
-                <div class="">
+                <div class="event-text">
                     <h3>TITLE HERE</h3>
                     <p>02/05/2023 | By Author</p>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus laborum consectetur atque nobis repellendus nisi quam blanditiis sequi, iure error...</p>
@@ -49,7 +49,7 @@
                 <div class="event-img">
                     <img src="{{ asset('img/men-discussing.jpg') }}" alt="ABECN logo">
                 </div>
-                <div class="">
+                <div class="event-text">
                     <h3>TITLE HERE</h3>
                     <p>02/05/2023 | By Author</p>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus laborum consectetur atque nobis repellendus nisi quam blanditiis sequi, iure error...</p>
@@ -57,21 +57,40 @@
                 </div>
             </article>
         </section>
-        <section>
+        <section class="sponsors mb-4">
             <div class="inner-container">
                 <h2>OUR SPONSORS</h2>
-                <div>
-                    {{-- Images --}}
+                <div class="flex overflow-x-scroll pb-10">
+                    <div class="flex flex-nowrap">
+                        <div class="inline-block px-3">
+                            <img src="{{ asset('img/london-eye.webp') }}" alt="ABECN logo">
+                        </div>
+                        <div class="inline-block px-3">
+                            <img src="{{ asset('img/london-eye.webp') }}" alt="ABECN logo">
+                        </div>
+                        <div class="inline-block px-3">
+                            <img src="{{ asset('img/london-eye.webp') }}" alt="ABECN logo">
+                        </div>
+                        <div class="inline-block px-3">
+                            <img src="{{ asset('img/london-eye.webp') }}" alt="ABECN logo">
+                        </div>
+                        <div class="inline-block px-3">
+                            <img src="{{ asset('img/london-eye.webp') }}" alt="ABECN logo">
+                        </div>
+                        <div class="inline-block px-3">
+                            <img src="{{ asset('img/london-eye.webp') }}" alt="ABECN logo">
+                        </div>
+                    </div>
                 </div>
                 <a href="#">MORE DETAILS</a>
-                @foreach ($users as $user)
+                <!-- @foreach ($users as $user)
         
                     <p><strong>First Name:</strong> {{ $user->first_name }}</p>
                     <p><strong>Group Name:</strong> {{ $user->group->name }}</p>
                     <p><strong>Membership Name:</strong> {{ $user->membership->name }}</p>
                     <br>
         
-                @endforeach
+                @endforeach -->
             </div>
         </section>
     </main>
