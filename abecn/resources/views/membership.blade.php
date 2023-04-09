184b4d839d37955303d564aac363e3a7bcdb537f
DeniseAI <deniseisabelo16@gmail.com>
2023-04-09 17:30:47 -0600

    Update membership.blade.php
    Editing and styling membership info page

--- abecn/resources/views/membership.blade.php
+++ abecn/resources/views/membership.blade.php
@@ -1,57 +1,38 @@
 <x-layout>
     <main>
-        <!-- <section class="membership-banner">
-            <div class="membership-text">
-                <h1>ABECN Membership</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-            </div>
-            <div class="img-overlap">
-
-            </div>
-        </section>
-        <section class="plans">
-            <h2>Choose Your Plan</h2>
-            <div class="plans-card">
-                <div class="individual">
-                    <a href="/register">Individual</a>
-                </div>
-                <div class="student">
-                    <a href="/register">Student</a>
-                </div>
-                <div class="price">
-
-                </div>
-                <ul class="plan-benefits">
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-                </ul>
-            </div>
-        </section> -->
-        
-        <div class="pricing-table-2 bg-gray-800 py-6 md:py-12">
-            <div class="container mx-auto px-4">
-
-                <div class="max-w-3xl mx-auto text-center">
-                    <h1 class="text-3xl md:text-4xl font-medium text-white mb-4 md:mb-6">ABECN Membership</h1>
-                    <p class="text-gray-500 xl:mx-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <div class="pricing-table-2 md:py-12">
+            <div>
+                <div class="membership">
+                    <div class="membership w-full mb-9 py-8 inner-container">
+                        <div class="membership-text">
+                            <h1 class="md:text-4xl text-white mb-4 md:mb-6">ABECN Membership</h1>
+                            <p class="text-white mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div class="membership-img">
+                            <div class="membership-img-top">
+                                <img class="w-3/4" src="{{ asset('img/meeting.png') }}" alt="Group Discussion">
+                            </div>
+                            <div class="membership-img-bottom">
+                                <img src="{{ asset('img/buildings-sky.jpg') }}" alt="Buildings against the sky">
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
-                <div class="pricing-plans lg:flex lg:-mx-4 mt-6 md:mt-12">
+                <div class="pricing-plans lg:flex lg:-mx-4 mt-12 md:mt-12">
 
                     {{-- Membership Plans--}}
                     @foreach ($memberships as $membership)
                     <div class="pricing-plan-wrap lg:w-1/2 my-4 md:my-6">
-                        <div class="pricing-plan border-t-4 border-solid border-white bg-white text-center max-w-sm mx-auto hover:border-indigo-600 transition-colors duration-300">
+                        <div class="pricing-plan border-t-4 border-solid border-white bg-sky-100 text-center max-w-sm mx-auto hover:border-indigo-600 transition-colors duration-300">
                         <div class="p-6 md:py-8">
-                            <h2 class="font-medium leading-tight text-2xl mb-2">{{ $membership->name() }}</h2>
+                            <h2 class="leading-tight mb-2">{{ $membership->name() }}</h2>
                         </div>
-                        <div class="pricing-amount bg-indigo-100 p-6 transition-colors duration-300">
-                            <div class=""><span class="text-4xl font-semibold">{{ $membership->price() }}</span> {{ $membership->abbreviation() }}</div>
+                        <div class="pricing-amount bg-slate-200 p-6 transition-colors duration-300">
+                            <div class=""><span class="font-semibold">{{ $membership->price() }}</span> {{ $membership->abbreviation() }}</div>
                         </div>
                         <div class="p-6">
-                            <h3 class="font-semibold leading-tight text-xl mb-2" >As a member you will benefit:</h3>
+                            <h3 class="font-semibold leading-tight mb-2" >As a member you will benefit:</h3>
                             <ul class="leading-loose">
                             <li>Network with industry leaders</li>
                             <li>Share innovation and technology</li>
@@ -60,7 +41,7 @@
                             <li>Your voice will be heard</li>
                             </ul>
                             <div class="mt-6 py-4">
-                            <a href="{{ route('register', ['membership' => $membership->stripeName()]) }}" class="px-6 py-3 bg-indigo-600 text-xl text-white py-2 px-6 rounded hover:bg-indigo-700 transition-colors duration-300">Become A Member</a>
+                            <a href="{{ route('register', ['membership' => $membership->stripeName()]) }}" class="dark-btn">Become A Member</a>
                             
                             </div>
                         </div>
