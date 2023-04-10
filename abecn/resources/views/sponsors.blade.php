78f3eaaefec06ea9659d4a4e9c1ff414526245c7
DeniseAI <deniseisabelo16@gmail.com>
2023-04-09 18:55:36 -0600

    Update sponsors.blade.php
    Editing and styling

--- abecn/resources/views/sponsors.blade.php
+++ abecn/resources/views/sponsors.blade.php
@@ -1,20 +1,30 @@
 <x-layout>
-    <div class="sponsor-page">
-        <h2>Sponsors</h2>
-        <div class="search-filter-wrapper">
-            <x-search>
-            </x-search>
-
-            <x-filter>
-            </x-filter>
+    <main>
+        <div class="sponsor-page">
+            <div class="sponsors-banner mb-9" style="height:17rem;">
+                <div class="sponsors-banner bg-cover bg-center flex items-center text-center justify-center align-middle h-full w-full;"> 
+                    <div class="banner-text inner-container">
+                        <h1>Sponsors</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="search-filter-wrapper inner-container flex items-center align-center">
+                <x-search>
+                </x-search>
+    
+                <x-filter>
+                </x-filter>
+            </div>
+            <div class="sponsors-wrapper inner-container mb-9">
+                <section class="sponsor-card">
+                    @foreach ($sponsors as $sponsor)
+                    <div class="mx-auto">
+                        <a href="/sponsors/{{ $sponsor->company_name }}"><h3>{{ $sponsor->company_name }}</h3></a>
+                        <img src="{{ $sponsor->img }}" alt="Photo">
+                    </div>
+                    @endforeach
+                </section>
+            </div>
         </div>
-        <div class="sponsors-wrapper">
-            <section class="sponsor-card">
-                @foreach ($sponsors as $sponsor)
-                    <a href="/sponsors/{{ $sponsor->company_name }}"><h3>{{ $sponsor->company_name }}</h3></a>
-                    <img src="{{ $sponsor->img }}" alt="Photo">
-                @endforeach
-            </section>
-        </div>
-    </div>
+    </main>
 </x-layout>
