80b4d0186400fd354a58ce6a8f2c5cfaa095af63
DeniseAI <deniseisabelo16@gmail.com>
2023-04-09 21:17:24 -0600

    Update memberdirectory.blade.php

--- abecn/resources/views/memberdirectory.blade.php
+++ abecn/resources/views/memberdirectory.blade.php
@@ -1,102 +1,113 @@
 <x-layout>
     <main>
-        <section class="contact-banner">
-            <h1>Contact</h1>
-        </section>
-        <section class="board-of-directors">
-            <div class="directors-left-row">
-                <div class="director-card">
-                    <div class="profile-img">
-                    </div>
-                    <div class="director-info">
-                        <h4>Name Here</h4>
-                        <h5>Position/Title Here</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                    </div>
-                </div>
-                <div class="director-card">
-                    <div class="profile-img">
-                    </div>
-                    <div class="director-info">
-                        <h4>Name Here</h4>
-                        <h5>Position/Title Here</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                    </div>
-                </div>
-                <div class="director-card">
-                    <div class="profile-img">
-                    </div>
-                    <div class="director-info">
-                        <h4>Name Here</h4>
-                        <h5>Position/Title Here</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                    </div>
-                </div>
-                <div class="director-card">
-                    <div class="profile-img">
-                    </div>
-                    <div class="director-info">
-                        <h4>Name Here</h4>
-                        <h5>Position/Title Here</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                    </div>
-                </div>
-                <div class="director-card">
-                    <div class="profile-img">
-                    </div>
-                    <div class="director-info">
-                        <h4>Name Here</h4>
-                        <h5>Position/Title Here</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                    </div>
+        <section class="member-directory-banner mb-9" style="height:17rem;">
+            <div class="member-directory-banner bg-cover bg-center flex items-center text-center justify-center align-middle h-full w-full;"> 
+                <div class="banner-text inner-container">
+                    <h1>Member Directory</h1>
                 </div>
             </div>
-            <div class="directors-right-row">
-                <div class="director-card">
-                    <div class="profile-img">
-                    </div>
-                    <div class="director-info">
-                        <h4>Name Here</h4>
-                        <h5>Position/Title Here</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                    </div>
+        </section>
+        <section class="members inner-container mb-9">
+            <div class="member-card mx-auto bg-slate-100 mb-4 text-center rounded-lg drop-shadow-lg">
+                <div class="member-img">
+                    <img class="w-24 h-24 mb-3 mx-auto rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Profile image"/>
                 </div>
-                <div class="director-card">
-                    <div class="profile-img">
-                    </div>
-                    <div class="director-info">
-                        <h4>Name Here</h4>
-                        <h5>Position/Title Here</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                    </div>
+                <div class="member-info">
+                    <h4 class="mb-1 text-3xl">Name Here</h4>
+                    <h5 class="uppercase">Position/Title Here</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
                 </div>
-                <div class="director-card">
-                    <div class="profile-img">
-                    </div>
-                    <div class="director-info">
-                        <h4>Name Here</h4>
-                        <h5>Position/Title Here</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                    </div>
+            </div>
+            <div class="member-card mx-auto bg-slate-100 mb-4 text-center rounded-lg drop-shadow-lg">
+                <div class="member-img">
+                    <img class="w-24 h-24 mb-3 mx-auto rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Profile image"/>
                 </div>
-                <div class="director-card">
-                    <div class="profile-img">
-                    </div>
-                    <div class="director-info">
-                        <h4>Name Here</h4>
-                        <h5>Position/Title Here</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                    </div>
+                <div class="member-info">
+                    <h4 class="mb-1 text-3xl">Name Here</h4>
+                    <h5 class="uppercase">Position/Title Here</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
                 </div>
-                <div class="director-card">
-                    <div class="profile-img">
-                    </div>
-                    <div class="director-info">
-                        <h4>Name Here</h4>
-                        <h5>Position/Title Here</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                    </div>
+            </div>
+            <div class="member-card mx-auto bg-slate-100 mb-4 text-center rounded-lg drop-shadow-lg">
+                <div class="member-img">
+                    <img class="w-24 h-24 mb-3 mx-auto rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Profile image"/>
                 </div>
+                <div class="member-info">
+                    <h4 class="mb-1 text-3xl">Name Here</h4>
+                    <h5 class="uppercase">Position/Title Here</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                </div>
+            </div>
+            <div class="member-card mx-auto bg-slate-100 mb-4 text-center rounded-lg drop-shadow-lg">
+                <div class="member-img">
+                    <img class="w-24 h-24 mb-3 mx-auto rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Profile image"/>
+                </div>
+                <div class="member-info">
+                    <h4 class="mb-1 text-3xl">Name Here</h4>
+                    <h5 class="uppercase">Position/Title Here</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                </div>
+            </div>
+            <div class="member-card mx-auto bg-slate-100 mb-4 text-center rounded-lg drop-shadow-lg">
+                <div class="member-img">
+                    <img class="w-24 h-24 mb-3 mx-auto rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Profile image"/>
+                </div>
+                <div class="member-info">
+                    <h4 class="mb-1 text-3xl">Name Here</h4>
+                    <h5 class="uppercase">Position/Title Here</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                </div>
+            </div>
+            <div class="member-card mx-auto bg-slate-100 mb-4 text-center">
+                <div class="member-img">
+                    <img class="w-24 h-24 mb-3 mx-auto rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Profile image"/>
+                </div>
+                <div class="member-info">
+                    <h4 class="mb-1">Name Here</h4>
+                    <h5>Position/Title Here</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                </div>
+            </div>
+            <div class="member-card mx-auto bg-slate-100 mb-4 text-center">
+                <div class="member-img">
+                    <img class="w-24 h-24 mb-3 mx-auto rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Profile image"/>
+                </div>
+                <div class="member-info">
+                    <h4 class="mb-1">Name Here</h4>
+                    <h5>Position/Title Here</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                </div>
+            </div>
+            <div class="member-card mx-auto bg-slate-100 mb-4 text-center">
+                <div class="member-img">
+                    <img class="w-24 h-24 mb-3 mx-auto rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Profile image"/>
+                </div>
+                <div class="member-info">
+                    <h4 class="mb-1">Name Here</h4>
+                    <h5>Position/Title Here</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                </div>
+            </div>
+            <div class="member-card mx-auto max-w-md bg-slate-100 mb-4 text-center">
+                <div class="member-img">
+                    <img class="w-24 h-24 mb-3 mx-auto rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Profile image"/>
+                </div>
+                <div class="member-info">
+                    <h4 class="mb-1">Name Here</h4>
+                    <h5>Position/Title Here</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                </div>
+            </div>
+            <div class="member-card mx-auto max-w-md bg-slate-100 mb-4 text-center">
+                <div class="member-img">
+                    <img class="w-24 h-24 mb-3 mx-auto rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Profile image"/>
+                </div>
+                <div class="member-info">
+                    <h4 class="mb-1">Name Here</h4>
+                    <h5>Position/Title Here</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                </div>
+            </div>
             </div>
         </section>
     </main>
