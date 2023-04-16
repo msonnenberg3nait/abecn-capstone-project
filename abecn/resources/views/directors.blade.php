8c9c6800f673cf7d60379704cc0abf7e77638d3e
DeniseAI <deniseisabelo16@gmail.com>
2023-04-15 22:25:26 -0600

    Create directors.blade.php

---
+++ abecn/resources/views/directors.blade.php
@@ -0,0 +1,85 @@
+<x-layout>
+    <main>
+        <section class="director-banner" style="height:17rem;">
+            <div class="directors-banner bg-cover bg-center flex items-center text-center justify-center align-middle h-full w-full;"> 
+                <div class="banner-text inner-container">
+                    <h2>Board of Directors</h2>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="directors inner-container md:flex flex-wrap gap-2">
+                <div class="director-card p-6 mx-auto bg-slate-100 mb-4 rounded-lg drop-shadow-lg basis-[48%]">    
+                    <div class="director-info">
+                        <h4 class="mb-1 text-3xl">Julien St-Pierre, PEng</h4>
+                        <h5 class="uppercase">President</h5>
+                        <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                    </div>
+                </div>
+                <div class="director-card p-6 mx-auto bg-slate-100 mb-4 rounded-lg drop-shadow-lg basis-[48%]">    
+                    <div class="director-info">
+                        <h4 class="mb-1 text-3xl">Amir Hassan, MSc, PE, PEng</h4>
+                        <h5 class="uppercase">Past President</h5>
+                        <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                    </div>
+                </div>
+                <div class="director-card p-6 mx-auto bg-slate-100 mb-4 rounded-lg drop-shadow-lg basis-[48%]">    
+                    <div class="director-info">
+                        <h4 class="mb-1 text-3xl">Lindsay Verhelst</h4>
+                        <h5 class="uppercase">Secretary</h5>
+                        <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                    </div>
+                </div>
+                <div class="director-card p-6 mx-auto bg-slate-100 mb-4 rounded-lg drop-shadow-lg basis-[48%]">    
+                    <div class="director-info">
+                        <h4 class="mb-1 text-3xl">Richard Lucid, CTR</h4>
+                        <h5 class="uppercase">Membership</h5>
+                        <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                    </div>
+                </div>
+                <div class="director-card p-6 mx-auto bg-slate-100 mb-4 rounded-lg drop-shadow-lg basis-[48%]">    
+                    <div class="director-info">
+                        <h4 class="mb-1 text-3xl">Garratt Grenier, RRO, CTR</h4>
+                        <h5 class="uppercase">Programming</h5>
+                        <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                    </div>
+                </div>
+                <div class="director-card p-6 mx-auto bg-slate-100 mb-4 rounded-lg drop-shadow-lg basis-[48%]">    
+                    <div class="director-info">
+                        <h4 class="mb-1 text-3xl">Ryan Asselstine</h4>
+                        <h5 class="uppercase">Treasurer</h5>
+                        <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                    </div>
+                </div>
+                <div class="director-card p-6 mx-auto bg-slate-100 mb-4 rounded-lg drop-shadow-lg basis-[48%]">    
+                    <div class="director-info">
+                        <h4 class="mb-1 text-3xl">Kevin McCunn, PEng</h4>
+                        <h5 class="uppercase">Technical Reviewer</h5>
+                        <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                    </div>
+                </div>
+                <div class="director-card p-6 mx-auto bg-slate-100 mb-4 rounded-lg drop-shadow-lg basis-[48%]">    
+                    <div class="director-info">
+                        <h4 class="mb-1 text-3xl">Jamie Murphy, PL(Eng)</h4>
+                        <h5 class="uppercase">Publication</h5>
+                        <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                    </div>
+                </div>
+                <div class="director-card p-6 mx-auto bg-slate-100 mb-4 rounded-lg drop-shadow-lg basis-[48%]">    
+                    <div class="director-info">
+                        <h4 class="mb-1 text-3xl">Christa van Dyk</h4>
+                        <h5 class="uppercase">Director</h5>
+                        <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                    </div>
+                </div>
+                <div class="director-card p-6 mx-auto bg-slate-100 mb-4 rounded-lg drop-shadow-lg basis-[48%]">    
+                    <div class="director-info">
+                        <h4 class="mb-1 text-3xl">Joe Mis</h4>
+                        <h5 class="uppercase">Director</h5>
+                        <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</x-layout>
