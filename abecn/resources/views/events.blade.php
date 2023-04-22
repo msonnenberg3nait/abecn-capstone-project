565ab85dbf9f86801c5fcc04aa7ba67bbdd8b519
DeniseAI <deniseisabelo16@gmail.com>
2023-04-22 15:59:50 -0600

    Editing layout styling

--- abecn/resources/views/events.blade.php
+++ abecn/resources/views/events.blade.php
@@ -1,19 +1,21 @@
 <x-layout>
     <x-slot name="title"><?php echo $title; ?></x-slot>
     <main>
-        <div class="events-banner">
-            <div class="events-banner w-full mb-9 py-8 inner-container">
-                <div class="events-text">
-                    <h2>Events</h2>
-                    <hr class="text-border w-[10%] mb-6">
-                    <p class="text-white">We offer a variety of events throughout the year including in-person training sessions, and industry conferences, to help you stay up-to-date on the latest trends and best practices in this important field. Our events are designed to provide you with a valuable learning experience and the opportunity to connect with industry professionals and fellow members of our council.</p>
-                </div>
-                <div class="events-img">
-                    <div class="events-img-top">
-                        <img src="{{ asset('img/singer-silhouette.webp') }}" alt="Silhouette of a singer performing with a blue background">
+        <div class="mt-16">
+            <div class="events-banner w-full mb-9 py-8">
+                <div class="events-banner-flex inner-container">
+                    <div class="events-text">
+                        <h2>Events</h2>
+                        <hr class="text-border w-[10%] mb-6">
+                        <p class="text-white">We offer a variety of events throughout the year including in-person training sessions, and industry conferences, to help you stay up-to-date on the latest trends and best practices in this important field. Our events are designed to provide you with a valuable learning experience and the opportunity to connect with industry professionals and fellow members of our council.</p>
                     </div>
-                    <div class="events-img-bottom">
-                        <img src="{{ asset('img/venue.webp') }}" alt="Interior view of a venue occupied with people">
+                    <div class="events-img">
+                        <div class="events-img-top">
+                            <img src="{{ asset('img/singer-silhouette.webp') }}" alt="Silhouette of a singer performing with a blue background">
+                        </div>
+                        <div class="events-img-bottom">
+                            <img src="{{ asset('img/venue.webp') }}" alt="Interior view of a venue occupied with people">
+                        </div>
                     </div>
                 </div>
             </div>
