7392762ac5ff90299755f6758cb1078d70764208
Marcus Sonnenberg <msonnenberg3@studentmail.nait.ca>
2023-04-23 23:21:26 -0600

    Updated stripe payment redirect

--- abecn/routes/web.php
+++ abecn/routes/web.php
@@ -41,6 +41,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/edit', [ProfileController::class, 'edit'])->name('edit');
     Route::get('/billing-portal', BillingPortalController::class)->name('billing-portal');
 });
 
